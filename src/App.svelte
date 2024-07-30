801852f139f8b685fb733bfc3ccb2a82174fb14f
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-07-30 16:38:59 +0200

    edited the App.svelte file so that the project has routes that lead to the relevant paths

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,29 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Router, Route } from 'svelte-routing';
+  import Header from './lib/Header.svelte';
+  import Filter from './lib/Filter.svelte';
+  import Sort from './lib/Sort.svelte';
+  import ProductList from './lib/ProductList.svelte';
+  import ProductDetail from './lib/ProductDetail.svelte';
+  import { productStore, fetchProducts } from './productStore';
+
+  import { onMount } from 'svelte';
+  onMount(async () => {
+    await fetchProducts();
+  });
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Header />
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Router>
+  <main class="container mx-auto mt-8">
+    <Route path="/">
+      <div class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center">
+        <Filter />
+        <Sort />
+      </div>
+      <ProductList />
+    </Route>
+    <Route path="product/:productId" component={ProductDetail} />
+  </main>
+</Router>
