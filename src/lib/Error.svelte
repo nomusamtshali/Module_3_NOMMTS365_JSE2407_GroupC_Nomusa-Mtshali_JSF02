17bae6920e07fcbaa8cb7eaaf07742a4ea374b17
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-07-31 16:57:26 +0200

    added some skeletons for the loading state of the cards and the product page

---
+++ src/lib/Error.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let message = "Data fetching failed, please check your network connection";
+  </script>
+
+  <div class="mt-28 text-red-500 text-xl font-bold">
+    {message}
+  </div>
+
+  <style></style>
