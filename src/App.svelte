e7ffdb44c881371308e33c48649a0debc169e551
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-08-01 13:15:59 +0200

    removed abstracted code from the App.svelte file

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,11 @@
 <script>
   import { Router, Route } from 'svelte-routing';
   import Header from './lib/Header.svelte';
-  import Filter from './lib/Filter.svelte';
-  import Sort from './lib/Sort.svelte';
-  import ProductList from './lib/ProductList.svelte';
   import ProductDetail from './lib/ProductDetail.svelte';
-  import { productStore, fetchProducts } from './productStore';
+  // import { productStore, fetchProducts } from './productStore';
 
   import { onMount } from 'svelte';
-  onMount(async () => {
-    await fetchProducts();
-  });
+  import ProductCard from './lib/ProductCard.svelte';
 </script>
 
 <Header />
@@ -18,12 +13,8 @@
 <Router>
   <main class="container mx-auto mt-8">
     <Route path="/">
-      <div class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center">
-        <Filter />
-        <Sort />
-      </div>
-      <ProductList />
+       <ProductCard/>
     </Route>
-    <Route path="product/:productId" component={ProductDetail} />
+    <Route path="products/:id" component={ProductDetail} />
   </main>
 </Router>
